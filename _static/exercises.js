02afcae28d8581a8277d329b1829da7009a1009b
Troels Henriksen <athas@sigkill.dk>
2022-04-13 12:59:18 +0200

    Fix exercise hack.

--- _static/exercises.js
+++ _static/exercises.js
@@ -1,13 +1,13 @@
 function toggleVisible (x) {
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
 
 window.onload = function () {
-    $('.solution>.first').each(function (index) {
+    $('.solution:first>.admonition-title').each(function (index) {
         solution = this.nextElementSibling;
         this.onclick = function() { toggleVisible(solution); };
     });
